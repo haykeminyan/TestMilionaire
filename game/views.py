b5aab0954ddad3bfdd90c6a7efb8566c1ccb20a2
Hayk Eminyan <43892880+haykeminyan@users.noreply.github.com>
2023-09-12 01:47:20 +0400

    version 0.3.2.1 prettier game.view

--- game/views.py
+++ game/views.py
@@ -1,7 +1,5 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Profile, Question, Answer
-from django.http import HttpResponse
-
 
 # Create your views here.
 
@@ -11,21 +9,26 @@ def login_page(request):
 
 
 def game(request, pk):
-    question = Question.objects.get(id=pk)
+    question = get_object_or_404(Question, pk=pk)
     answer = Answer.objects.get(question_id=request.user.id)
+    user = Profile.objects.get(id=request.user.id)
+
+    print(question.id)
 
     data = {
-        "user": Profile.objects.get(id=request.user.id),
-        "points": Profile.objects.get(id=request.user.id).points,
+        "user": user,
+        "points": user.points,
         "question_id": question.id,
         "question": question.question,
         "array_of_answers": answer.array_of_answers,
         "correct_answer": answer.correct_answer,
     }
-    print(data)
+
+    next_page = question.id + 1
+
     return render(
         request,
         "game/game.html",
-        {"data": data, "next_page": str(int(pk)+1), "previous_page": str(int(pk)-1)},
+        {"data": data, "pk": next_page},
     )
 
